f28849506f7a25480d0f0521fd206670652d9415
Mustafa1906 <72093036+Mustafa1906@users.noreply.github.com>
2020-10-13 21:56:58 +0100

    add it new class

--- src/test/java/HelloGroup3.java
+++ src/test/java/HelloGroup3.java
@@ -2,6 +2,7 @@ public class HelloGroup3 {
 
     public static void main(String[] args) {
         System.out.println("Hello Everyone lets do the project together");
+        System.out.println("Hello everyone");
 
         //added some Changes
     }

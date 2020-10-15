bb524c8e076585ec6264b85085f5ffe17f4e0510
Ercan Cotenoglu <ercancotenoglu@gmail.com>
2020-10-15 06:28:46 +0100

    HW UPDATE

---
+++ src/test/java/HW.java
@@ -0,0 +1,35 @@
+public class HW {
+
+    public static void main(String[] args) {
+
+
+   /*
+        ## Please go to the homepage of our project
+## add one file with your name directly from GitHub
+  ## to this repository
+
+        In your team project please do following
+        1. make sure you have cloned the team project
+        in your intelliJ
+                (DO NOT FORK, YOU HAVE ACCESS TO PUSH ALREADY)
+        2. create a new branch with your name followed by 1
+        Ali-1
+        3. Add new file with name YourName.java
+        commit your change to the your branch
+        4. push your change to remote repo
+        5. open a pull request ,
+        6, everybody jump in and discuss and comment on
+        each others pull request
+        7. eventually merge the changes into master
+        -----------
+                Always pull first , before commit and push
+        to stay in sync with remote repo
+        while working in team environment
+
+       */
+
+
+
+
+    }
+}

5a41424b0c768396aa7a9354361c4126e317b370
EU3 Batch <yavuzyesilyurt3@gmail.com>
2020-10-16 21:28:16 +0100

    fork and clone difference

--- src/test/java/YavuzNewBranch.java
+++ src/test/java/YavuzNewBranch.java
@@ -2,5 +2,8 @@ public class YavuzNewBranch {
     public static void main(String[] args) {
 
         System.out.println("creating class in my branch");
+
+        // if you are collaboretor of project you just clone
+        // need fork just if you are no related with any project
     }
 }

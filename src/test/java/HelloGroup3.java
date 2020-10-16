fed25f3835455ca92a41ec7c97ecd7d10110faba
Rajeev Dahal <rajeev88@gmail.com>
2020-10-16 18:04:42 +0100

    added some lines

--- src/test/java/HelloGroup3.java
+++ src/test/java/HelloGroup3.java
@@ -3,6 +3,10 @@ public class HelloGroup3 {
     public static void main(String[] args) {
         System.out.println("Hello Everyone lets do the project together");
 
+        System.out.println("Mentoring session");
+
         //added some Changes
+
+
     }
 }

5c92ac43cfa6e5eaae5d6bf1f3813133de5eaab6
zuhalsWonderland <72165041+zuhalsWonderland@users.noreply.github.com>
2020-10-17 23:29:57 +0100

    Trying to cause a merge conflict:))

--- src/main/java/newCAlssUnderRajeev2.java
+++ src/main/java/newCAlssUnderRajeev2.java
@@ -1,5 +1,5 @@
 public class newCAlssUnderRajeev2 {
     public static void main(String[] args) {
-        System.out.println("new class added");
+        System.out.println("new class added and doing some changes here to create a merge conflict:))");
     }
 }

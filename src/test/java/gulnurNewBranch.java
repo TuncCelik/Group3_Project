f3f166a0fbec45c861f80acd8af436b439d9caf6
“GULNURKIRCAER” <gulnur1984@yahoo.com>
2020-10-18 23:32:14 +0100

    new stuff

--- src/test/java/gulnurNewBranch.java
+++ src/test/java/gulnurNewBranch.java
@@ -4,9 +4,12 @@ public class gulnurNewBranch {
 
     public static void main(String[] args) {
         System.out.println("new branch");
-        System.out.println("I think I got topic");
+
         System.out.println("but I am not sure");
         //added more changes
+        System.out.println("I think I got topic");
+
+
 
 
 

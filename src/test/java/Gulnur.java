00133727486e6041dd6edd63222b8393e7e8e979
“GULNURKIRCAER” <gulnur1984@yahoo.com>
2020-10-19 10:00:58 +0100

    GULNUR-3

--- src/test/java/Gulnur.java
+++ src/test/java/Gulnur.java
@@ -3,6 +3,10 @@ public class Gulnur{
     public static void main(String[] args) {
        
         System.out.println("Hello friends");
+        System.out.println("I got it");
+        System.out.println("I created several branches");
+        System.out.println("I got it");
+        System.out.println("I created several branches");
 
     }
 }

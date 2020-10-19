8f77974c21e6ecaa5341eeca4532340b70983295
“GULNURKIRCAER” <gulnur1984@yahoo.com>
2020-10-19 09:55:40 +0100

    new stuff

--- src/test/java/Gulnur.java
+++ src/test/java/Gulnur.java
@@ -4,13 +4,20 @@ public class Gulnur {
 
         System.out.println("Hello friends");
         System.out.println("Hello dear");
+        System.out.println("I got it");
+        System.out.println("I created several branches");
+
+
+        System.out.println("I got it");
+        System.out.println("I created several branches");
+
+
         System.out.println("I think I got topic");
         System.out.println("I got it");
         System.out.println("I created several branches");
 
         System.out.println("I got it");
         System.out.println("I created several branches");
-        System.out.println("I got it");
-        System.out.println("I created several branches");
+
     }
 }

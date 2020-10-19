3e756a87a9cd917b06eaf7067e8266b393781c8f
GULNURKIRCAER <72072437+GULNURKIRCAER@users.noreply.github.com>
2020-10-19 10:55:15 +0100

    Update Gulnur.java

--- src/test/java/Gulnur.java
+++ src/test/java/Gulnur.java
@@ -3,6 +3,10 @@ public class Gulnur{
     public static void main(String[] args) {
        
         System.out.println("Hello friends");
+        
+        System.out.println("but I am not sure");
+        //added more changes
+        System.out.println("I think I got topic");
 
     }
 }
